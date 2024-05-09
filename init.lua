54bb16af22a53e283ab402b1da0a17d1c931bf1e
DumbDave961 <144002335+DumbDave961@users.noreply.github.com>
2024-05-08 21:13:04 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,19 +1,23 @@
 --grinder recipes
 local grindings = {
-	{"default:obsidian",                   "default:obsidian_shard 9", 13},
-	{"default:obsidian_block",             "default:obsidian_shard 9", 13},
-	{"default:obsidianbrick",              "default:obsidian_shard 9", 13},
-	{"default:obsidian_glass",             "default:obsidian_shard", 5},
-	{"bucket:bucket_empty",                "elepower_dynamics:steel_dust 3", 7},
-	{"boats:boat",                         "elepower_dynamics:wood_dust 4", 9},
+	{"default:obsidian",        "default:obsidian_shard 9", 13},
+	{"default:obsidian_block",  "default:obsidian_shard 9", 13},
+	{"default:obsidianbrick",   "default:obsidian_shard 9", 13},
+	{"default:obsidian_glass",  "default:obsidian_shard", 5},
+	{"bucket:bucket_empty",     "elepower_dynamics:steel_dust 3", 7},
+	{"doors:door_steel",        "elepower_dynamics:steel_dust 6", 10},
+	{"default:sign_wall_steel", "elepower_dynamics:steel_dust 2", 6},
+	{"doors:trapdoor_steel",    "elepower_dynamics:steel_dust 4"},
+	{"boats:boat",              "elepower_dynamics:wood_dust 4", 8},
+	{"default:bookshelf",       "elepower_dynamics:wood_dust 4", 8},
 }
 
 if minetest.get_modpath("technic") then
 	elepm.register_craft({type = "compress", recipe = {"default:desert_sand", "technic:stone_dust"}, output = "default:desert_stone 2", time = 2})
 	
 	table.insert(grindings, {"basic_materials:carbon_steel_bar 2",          "technic:carbon_steel_dust", 5})
-	table.insert(grindings, {"default:desert_sandstone_block",              "default:desert_sand 2", 8})
-	table.insert(grindings, {"default:desert_sandstone_brick",              "default:desert_sand 2", 8})
+	table.insert(grindings, {"default:desert_sandstone_block",              "default:desert_sand 2", 6})
+	table.insert(grindings, {"default:desert_sandstone_brick",              "default:desert_sand 2", 6})
 	table.insert(grindings, {"stairs:slab_desert_sandstone",                "default:desert_sand", 5})
 	table.insert(grindings, {"stairs:slab_desert_sandstone_block",          "default:desert_sand", 5})
 	table.insert(grindings, {"stairs:slab_desert_sandstone_brick",          "default:desert_sand", 5})
@@ -26,6 +30,7 @@ if minetest.get_modpath("technic") then
 	table.insert(grindings, {"stairs:stair_outer_desert_sandstone 4",       "default:desert_sand 5", 9})
 	table.insert(grindings, {"stairs:stair_outer_desert_sandstone_block 4", "default:desert_sand 5", 9})
 	table.insert(grindings, {"stairs:stair_outer_desert_sandstone_brick 4", "default:desert_sand 5", 9})
+	table.insert(grindings, {"basic_materials:cement_block 3",              "default:dirt", 5})
 	table.insert(grindings, {"basic_materials:gold_strip 6",                "technic:gold_dust", 5})
 	table.insert(grindings, {"stairs:slab_cobble 2",                        "default:gravel", 5})
 	table.insert(grindings, {"stairs:stair_cobble 4",                       "default:gravel 3", 7})
@@ -36,16 +41,16 @@ if minetest.get_modpath("technic") then
 	table.insert(grindings, {"default:sandstonebrick",                      "default:sand 2", 6})
 	table.insert(grindings, {"stairs:slab_sandstone",                       "default:sand", 5})
 	table.insert(grindings, {"stairs:slab_sandstone_block",                 "default:sand", 5})
-	table.insert(grindings, {"stairs:slab_sandstonebrick",                 "default:sand", 5})
+	table.insert(grindings, {"stairs:slab_sandstonebrick",                  "default:sand", 5})
 	table.insert(grindings, {"stairs:stair_sandstone 4",                    "default:sand 6", 10})
 	table.insert(grindings, {"stairs:stair_sandstone_block 4",              "default:sand 6", 10})
-	table.insert(grindings, {"stairs:stair_sandstonebrick 4",              "default:sand 6", 10})
+	table.insert(grindings, {"stairs:stair_sandstonebrick 4",               "default:sand 6", 10})
 	table.insert(grindings, {"stairs:stair_inner_sandstone 4",              "default:sand 7", 11})
 	table.insert(grindings, {"stairs:stair_inner_sandstone_block 4",        "default:sand 7", 11})
-	table.insert(grindings, {"stairs:stair_inner_sandstonebrick 4",        "default:sand 7", 11})
+	table.insert(grindings, {"stairs:stair_inner_sandstonebrick 4",         "default:sand 7", 11})
 	table.insert(grindings, {"stairs:stair_outer_sandstone 4",              "default:sand 5", 9})
 	table.insert(grindings, {"stairs:stair_outer_sandstone_block 4",        "default:sand 5", 9})
-	table.insert(grindings, {"stairs:stair_outer_sandstonebrick 4",        "default:sand 5", 9})
+	table.insert(grindings, {"stairs:stair_outer_sandstonebrick 4",         "default:sand 5", 9})
 	table.insert(grindings, {"default:silver_sandstone_block",              "default:silver_sand 2", 6})
 	table.insert(grindings, {"default:silver_sandstone_brick",              "default:silver_sand 2", 6})
 	table.insert(grindings, {"stairs:slab_silver_sandstone",                "default:silver_sand", 5})
@@ -62,6 +67,7 @@ if minetest.get_modpath("technic") then
 	table.insert(grindings, {"stairs:stair_outer_silver_sandstone_brick 4", "default:silver_sand 5", 9})
 	table.insert(grindings, {"basic_materials:stainless_steel_bar 2",       "technic:stainless_steel_dust", 5})
 	table.insert(grindings, {"basic_materials:stainless_steel_strip 6",     "technic:stainless_steel_dust", 5})
+	table.insert(grindings, {"default:furnace",                             "technic:stone_dust", 5})
 	table.insert(grindings, {"default:stone_block",                         "technic:stone_dust", 5})
 	table.insert(grindings, {"default:stonebrick",                          "technic:stone_dust", 5})
 	table.insert(grindings, {"stairs:slab_stone 2",                         "technic:stone_dust", 5})
@@ -106,13 +112,13 @@ else
 	table.insert(grindings, {"stairs:slab_sandstone_brick",                 "default:sand 2", 6})
 	table.insert(grindings, {"stairs:stair_sandstone 2",                    "default:sand 6", 10})
 	table.insert(grindings, {"stairs:stair_sandstone_block 2",              "default:sand 6", 10})
-	table.insert(grindings, {"stairs:stair_sandstonebrick 2",              "default:sand 6", 10})
+	table.insert(grindings, {"stairs:stair_sandstonebrick 2",               "default:sand 6", 10})
 	table.insert(grindings, {"stairs:stair_inner_sandstone 2",              "default:sand 7", 11})
 	table.insert(grindings, {"stairs:stair_inner_sandstone_block 2",        "default:sand 7", 11})
-	table.insert(grindings, {"stairs:stair_inner_sandstonebrick 2",        "default:sand 7", 11})
+	table.insert(grindings, {"stairs:stair_inner_sandstonebrick 2",         "default:sand 7", 11})
 	table.insert(grindings, {"stairs:stair_outer_sandstone 2",              "default:sand 5", 9})
 	table.insert(grindings, {"stairs:stair_outer_sandstone_block 2",        "default:sand 5", 9})
-	table.insert(grindings, {"stairs:stair_outer_sandstonebrick 2",        "default:sand 5", 9})
+	table.insert(grindings, {"stairs:stair_outer_sandstonebrick 2",         "default:sand 5", 9})
 	table.insert(grindings, {"default:silver_sandstone_block",              "default:silver_sand 4", 8})
 	table.insert(grindings, {"default:silver_sandstone_brick",              "default:silver_sand 4", 8})
 	table.insert(grindings, {"stairs:slab_silver_sandstone",                "default:silver_sand 2", 6})
